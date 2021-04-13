a90a5bf66d471c6a1f773680426b676d62c37362
Adrian <Adrian@DESKTOP-TKKPDNE>
2021-04-13 12:17:14 +0200

    .

---
+++ GestorTareas/src/Tareas.java
@@ -0,0 +1,18 @@
+
+public class Tareas {
+	private String Titulo;
+
+	public Tareas(String titulo) {
+		Titulo = titulo;
+	}
+
+	public String getTitulo() {
+		return Titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+	
+	
+}
